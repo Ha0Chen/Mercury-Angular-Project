a536bdb846e6aba4c6287799a3db19ed50f62a05
Hao Chen <hao.chen.duke@gmail.com>
2023-07-17 16:55:12 -0400

    test jenkins again

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -12,7 +12,7 @@ import {AuthService} from "../shared/services/auth.service";
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-  title:String = "E-ducateeee";
+  title:String = "E-ducateeeee";
   @Input() deviceXs?:boolean;
   constructor(public dialog: MatDialog,
               public auth: AuthService) {}
